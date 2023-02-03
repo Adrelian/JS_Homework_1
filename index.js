c7eb25bd195299d55baaa52bcfacb544be47b151
Anatolii <feelmysmell@yandex.ru>
2023-02-03 18:48:25 +0500

    Задание 1

---
+++ index.js
@@ -0,0 +1,10 @@
+// Задача 1
+let temperatureCelsi = 15;
+let temperatureFarengeit = (9 / 5) * temperatureCelsi + 32;
+
+console.log(`${temperatureCelsi} по Цельсию равно ${temperatureFarengeit}`);
+
+// Задача 2
+let firstName = "Anatolii";
+let admin = firstName;
+console.log(admin)
